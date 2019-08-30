c5f11bb79039f71169751e7d9f7754237687bd95
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-30 17:47:08 -0400

    removed old routes

--- srv/routes/allUsers.js
+++
@@ -1,9 +0,0 @@
-const { getTable } = require('../db/getTable');
-
-module.exports = {
-  getAllUsers: (req, res) => {
-    const query = 'SELECT username FROM user';
-    getTable(query)
-      .then(data => res.json(data));
-  },
-};
