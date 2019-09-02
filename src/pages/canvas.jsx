18c4718eb0067c3415e231bb9983fa27b6c498de
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-09-02 09:58:07 -0400

    added interactivity with the mouse position. Bubbles increase in size when in proximity to the mouse

--- src/pages/canvas.jsx
+++ src/pages/canvas.jsx
@@ -13,12 +13,16 @@ const circle = (x, y, radius, colour, dx, dy) => {
     x,
     y,
     radius,
+    extraRadius: 0,
     colour,
     dx,
     dy,
-    draw() {
+    mouseProximity: 0,
+    draw(mouse) {
+      this.getMouseProximity(mouse);
+      this.updateRadius();
       c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.arc(this.x, this.y, this.radius + this.extraRadius, 0, Math.PI * 2, false);
       c.strokeStyle = colour;
       c.fillStyle = colour;
       c.stroke();
@@ -39,16 +43,43 @@ const circle = (x, y, radius, colour, dx, dy) => {
       this.x += this.dx;
       this.y += this.dy;
     },
+    getMouseProximity(mouse) {
+      this.mouseProximity = Math.sqrt(((this.x - mouse.x) ** 2) + ((this.y - mouse.y) ** 2));
+    },
+    updateRadius() {
+      const interactionRadius = 50;
+      if (this.mouseProximity < interactionRadius) {
+        this.extraRadius = 50 * ((interactionRadius - this.mouseProximity) / interactionRadius);
+      } else {
+        this.extraRadius = 0;
+      }
+    },
   });
 };
-const circles = [];
-const colours = ['yellow', 'orange', 'black'];
 
-for (let i = 0; i < 100; i += 1) {
-  const radius = 30;
+const mouse = {
+  x: undefined,
+  y: undefined,
+  updatePosition(event) {
+    this.x = event.x;
+    this.y = event.y;
+  },
+};
+
+window.addEventListener('mousemove', (event) => {
+  mouse.updatePosition(event);
+});
+
+const circles = [];
+const colours = ['#E27D60', '#85DCBA', '#E8A87C', '#C38D9E', '#41B3A3'];
+
+for (let i = 0; i < 200; i += 1) {
+  const radius = 5;
   const spawnx = Math.random() * (innerWidth - radius * 2) + radius;
   const spawny = Math.random() * (innerHeight - radius * 2) + radius;
-  const circ = circle(spawnx, spawny, radius, colours[i % 3], (Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2);
+  const spawndx = (Math.random() - 0.5) * 3;
+  const spawndy = (Math.random() - 0.5) * 3;
+  const circ = circle(spawnx, spawny, radius, colours[i % colours.length], spawndx, spawndy);
   circles.push(circ);
 }
 
@@ -56,7 +87,7 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   circles.forEach((circ) => {
-    circ.draw();
+    circ.draw(mouse);
     circ.updatePosition();
   });
 }
